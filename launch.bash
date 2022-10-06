9b3da67ea7b3def8a2da94ed568bfc4540074eac
supplient <437566830@qq.com>
2022-10-06 08:54:09 +0800

    Seems streamlit not working if use daemon.

--- launch.bash
+++ launch.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /root/inpaint
 /root/miniconda3/bin/python /root/inpaint/init.py
-/root/miniconda3/bin/python -m streamlit run /root/inpaint/st_main.py
+nohup /root/miniconda3/bin/python -m streamlit run /root/inpaint/st_main.py &
